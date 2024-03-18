0539323e2b5ae7754561af8810fd210875f7106d
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-03-18 16:12:44 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
 import Navigation from "./components/Navigation/Navigation";
-
 import "./App.css";
+import { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Suspense fallback={<p>Loading Page...</p>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
+import clsx from "clsx";
+
+export default function Navigation() {
+  const buildNavLinkClass = ({ isActive }) => {
+    return clsx(css.link, isActive && css.isActive);
+  };
+  return (
+    <div>
+      <nav>
+        <NavLink to="/" className={buildNavLinkClass}>
+          HomePage
+        </NavLink>
+        <NavLink to="movies" className={buildNavLinkClass}>
+          MoviesPage
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
