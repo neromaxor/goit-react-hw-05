2d2a2780af614f83896a4febda8f1cfb438f89ac
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-03-27 21:18:23 +0200

    update

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -5,50 +5,54 @@ import { searchByQuery } from "../../fetchApiFilm";
 import css from "./MoviesPage.module.css";
 
 export default function MoviesPage() {
-  const [params, setParams] = useSearchParams();
+  const [params] = useSearchParams();
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [errorNotFound, setErrorNotFound] = useState(false);
-  const query = params.get("query") ?? "";
-
-  const changeQuery = (newQuery) => {
-    params.set("query", newQuery);
-    setParams(params);
-  };
+  const [searchValue, setSearchValue] = useState(params.get("query") || "");
 
   useEffect(() => {
-    async function getMovies() {
-      try {
-        setError(false);
-        setLoading(true);
-        const data = await searchByQuery(query);
-        setMovies(data.results);
-        if (data.results.length === 0) {
-          setErrorNotFound(true);
-        } else {
-          setErrorNotFound(false);
-        }
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
+    setSearchValue(params.get("query") || "");
+  }, [params]);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const inputValue = searchValue.trim();
+    if (inputValue === "") {
+      return alert("Please enter your query");
     }
-    getMovies();
-  }, [query]);
+
+    try {
+      setErrorNotFound(false);
+      setError(false);
+      setLoading(true);
+      const data = await searchByQuery(inputValue);
+      setMovies(data.results);
+      if (data.results.length === 0) {
+        setErrorNotFound(true);
+      }
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className={css.container}>
       {loading && <h4 className={css.loading}>Loading...</h4>}
-      <form>
+      <form onSubmit={handleSearch}>
         <input
           type="text"
           name="search"
-          value={query}
-          onChange={(e) => changeQuery(e.target.value)}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
           className={css.searchInput}
         />
+        <button type="submit" className={css.searchButton}>
+          Search
+        </button>
       </form>
       {errorNotFound && (
         <h4 className={css.notFound}>Sorry, results not found</h4>
